b7d9f044c38291660977355770b8b0b9c0bff9b7
Hashiq-M <mhashiq110@gmail.com>
2024-04-02 19:23:49 +0530

    Login

---
+++ src/Login/Login.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+import "./Login.css";
+function Login() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </div>
+          <span>or use your email password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget Your Password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back to PIXEL MATRIX!</h1>
+            <p>Enter your details to dive into your MEMORIES</p>
+            <button className="hidden" id="login" onClick={handleLoginClick}>
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Register with PiXEL MATRIX to use all of its features</p>
+            <button
+              className="hidden"
+              id="register"
+              onClick={handleRegisterClick}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
